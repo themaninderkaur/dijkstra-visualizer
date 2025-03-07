a8279816de0ec76c52c4a760c2da1631ab91bc10
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-03-06 21:35:25 -0500

    Update dijkstra.h

--- src/dijkstra.h
+++ src/dijkstra.h
@@ -1,1 +1,10 @@
+ // dijkstra.h
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
 
+#include "graph.h"
+
+void dijkstra(Graph* graph, int startNode, int* distances, int* previous);
+void printShortestPath(int* previous, int startNode, int endNode);
+
+#endif // DIJKSTRA_H

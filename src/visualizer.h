ac4c74eadf42060b7fb51d281f25949264d372c8
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-03-06 21:36:04 -0500

    Update visualizer.h

--- src/visualizer.h
+++ src/visualizer.h
@@ -1,1 +1,10 @@
+ // visualizer.h
+#ifndef VISUALIZER_H
+#define VISUALIZER_H
 
+#include "graph.h"
+
+void visualizeGraph(Graph* graph);
+void visualizePath(int* previous, int startNode, int endNode);
+
+#endif // VISUALIZER_H

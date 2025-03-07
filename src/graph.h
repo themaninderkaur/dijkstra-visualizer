f3ea76d7a1d06235f34bf603155904adce5a64c5
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-03-06 21:34:51 -0500

    Update graph.h

--- src/graph.h
+++ src/graph.h
@@ -1,1 +1,28 @@
+ // graph.h
+#ifndef GRAPH_H
+#define GRAPH_H
 
+#include <limits.h>
+
+typedef struct Edge {
+    int destination;
+    int weight;
+    struct Edge* next;
+} Edge;
+
+typedef struct Node {
+    int id;
+    Edge* edges; // List of edges
+} Node;
+
+typedef struct Graph {
+    int numNodes;
+    Node* nodes; // Array of nodes
+} Graph;
+
+
+Graph* createGraph(int numNodes);
+void addEdge(Graph* graph, int src, int dest, int weight);
+void freeGraph(Graph* graph);
+
+#endif // GRAPH_H

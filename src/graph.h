62529d757db28936b3811c5b8f3d961817dec590
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-04-12 20:54:39 -0400

    shorten graph.h

--- src/graph.h
+++ src/graph.h
@@ -1,28 +1,12 @@
- // graph.h
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#include <limits.h>
-
-typedef struct Edge {
-    int destination;
-    int weight;
-    struct Edge* next;
-} Edge;
-
-typedef struct Node {
-    int id;
-    Edge* edges; // List of edges
-} Node;
-
-typedef struct Graph {
-    int numNodes;
-    Node* nodes; // Array of nodes
+typedef struct {
+    int numVertices;
+    int** adjMatrix;
 } Graph;
 
-
-Graph* createGraph(int numNodes);
-void addEdge(Graph* graph, int src, int dest, int weight);
+Graph* createGraph(int vertices);
 void freeGraph(Graph* graph);
 
 #endif // GRAPH_H

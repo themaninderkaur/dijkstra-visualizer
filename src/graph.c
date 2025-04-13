f31c23a087a620dc9a5437983e26eee3dda3aedc
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-04-12 20:55:09 -0400

    graph.c test1

--- src/graph.c
+++ src/graph.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "graph.h"
 
+Graph* createGraph(int vertices) {
+    Graph* graph = (Graph*)malloc(sizeof(Graph));
+    graph->numVertices = vertices;
+
+    graph->adjMatrix = (int**)malloc(vertices * sizeof(int*));
+    for (int i = 0; i < vertices; i++) {
+        graph->adjMatrix[i] = (int*)malloc(vertices * sizeof(int));
+        for (int j = 0; j < vertices; j++) {
+            graph->adjMatrix[i][j] = (i == j) ? 0 : INT_MAX;
+        }
+    }
+
+    return graph;
+}
+
+void freeGraph(Graph* graph) {
+    for (int i = 0; i < graph->numVertices; i++) {
+        free(graph->adjMatrix[i]);
+    }
+    free(graph->adjMatrix);
+    free(graph);
+}

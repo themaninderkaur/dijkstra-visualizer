4ea07f4eb19e374696c2a8730eda0d290f87ede7
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2025-04-12 20:55:55 -0400

    main.c test1

--- src/main.c
+++ src/main.c
@@ -4,26 +4,27 @@
  * 2025
  */
 
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+#include "dijkstra.h"
+#include "graph.h"
 
-/*
-* adds an edge
-*/
-
-void addEdge() {
-  
-}
-
-/*
-* shortest path to all other vertices
-*/
-
-void shortestPath() {
-  
+void initializeGraph(Graph* graph) {
+    // Example: Add edges to the graph
+    graph->adjMatrix[0][1] = 4;
+    graph->adjMatrix[0][2] = 1;
+    graph->adjMatrix[1][2] = 2;
+    graph->adjMatrix[1][3] = 5;
+    graph->adjMatrix[2][3] = 8;
 }
 
 int main() {
-
-  
+    int vertices = 4;
+    Graph* graph = createGraph(vertices);
+    initializeGraph(graph);
+    
+    dijkstra(graph, 0);
+    
+    freeGraph(graph);
+    return 0;
 }
